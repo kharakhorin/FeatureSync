d091d0baf071cdcf81748ee9408c4ae828668b4e
kharakhorin <kharakhorin@gmail.com>
2020-03-13 15:49:30 +0500

    Update CommandOptions.cs

--- FeatureSync/CommandOptions.cs
+++ FeatureSync/CommandOptions.cs
@@ -4,11 +4,11 @@ namespace FeatureSync
 {
     class CommandOptions
     {
-        [Option('f', "fearuresPath", Required = true, HelpText = "Путь к feature файлам")]
+        [Option('f', "features", Required = true, HelpText = "Feature files directory")]
         public string FearuresPath { get; set; }
-        [Option('s', "serverTfs", Required = true, HelpText = "URL TFS сервера")]
+        [Option('s', "server", Required = true, HelpText = "TFS server URL")]
         public string ServerTfs { get; set; }
-        [Option('t', "token", Required = true, HelpText = "Токен авторизации в TFS")]
+        [Option('t', "token", Required = true, HelpText = "Personal Access Token TFS")]
         public string Token { get; set; }
 
         public override string ToString()
